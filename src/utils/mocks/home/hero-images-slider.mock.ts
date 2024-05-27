dda463e91859953271232184e2dbf9e2c7af3724
marckender <makender103@gmail.com>
2024-05-27 00:29:08 -0300

    feat: homePage Hero

---
+++ src/utils/mocks/home/hero-images-slider.mock.ts
@@ -0,0 +1,14 @@
+
+import slide1 from '../../../../public/assets/hero/slide1.svg'
+import slide2 from '../../../../public/assets/hero/slide2.svg'
+
+const heroImagesMock = [
+    slide1,
+    slide2,
+    slide1,
+    slide2,
+    slide1,
+    slide2,
+];
+
+export default heroImagesMock;
