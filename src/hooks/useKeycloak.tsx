4db15389dd46510bfe469565e1d83f5f2eec171a
marckender <makender103@gmail.com>
2024-08-15 23:55:06 -0300

    refactor: keycloak authentication

--- src/hooks/useKeycloak.tsx
+++ src/hooks/useKeycloak.tsx
@@ -1,63 +1,71 @@
-import Keycloak, { KeycloakInstance } from 'keycloak-js';
-import { createContext, useContext, useState, useEffect } from 'react';
+import keycloak from "@/libs/pkg/keycloak";
+import { deleteCookie, getCookie, setCookie } from "cookies-next";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
-export interface KeycloakContextType {
-  keycloak: KeycloakInstance | null;
-  authenticated: boolean;
-  isLoading: boolean;
-  logout: () => void;
-  user: any;
-  handleLogin: () => void;
-}
+const AuthContext = createContext({
+  isAuthenticated: false,
+  token: "",
+  user: {} as any,
+  logout: () => {},
+  login: () => {},
+  handleLogin: () => {},
+});
 
-const KeycloakContext = createContext<KeycloakContextType>({} as KeycloakContextType);
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+const [token, setToken] = useState<string>("");
+  const isRun = useRef(false);
 
-export function KeycloakProvider({ children }: { children: React.ReactNode }) {
-  const [keycloak, setKeycloak] = useState<KeycloakInstance | null>(null);
-  const [authenticated, setAuthenticated] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [user, setUser] = useState<any>(null);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    const storedAuthenticated = localStorage.getItem('authenticated');
-
-    if (storedUser && storedAuthenticated === 'true') {
-      setUser(JSON.parse(storedUser));
-      setAuthenticated(true);
-      setIsLoading(false);
-    }
-
-    const keycloakInstance = new Keycloak({
-      url: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
-      realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM as string,
-      clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID as string,
-    });
-
-    setKeycloak(keycloakInstance);
-    keycloakInstance.init({
-      onLoad: 'check-sso',
-      checkLoginIframe: true,
-      flow: 'hybrid',
-      silentCheckSsoRedirectUri: window.location.origin + '/',
-    }).then((authenticated: any)=> {
-      if (authenticated) {
-        const userProfile = keycloakInstance.tokenParsed;
-        setUser(userProfile);
-        setAuthenticated(true);
-        localStorage.setItem('user', JSON.stringify(userProfile));
-        localStorage.setItem('authenticated', 'true');
-      } else {
-        localStorage.removeItem('user');
-        localStorage.removeItem('authenticated');
-        setAuthenticated(false);
-        setUser(null);
+  const getUserInfo = async (token:string) => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_KEYCLOAK_URL}/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/protocol/openid-connect/userinfo`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-      setIsLoading(false);
-    }).catch((error:any) => {
-      console.error('Failed to initialize Keycloak:', error);
-    });
-    setIsLoading(false);
+    );
+
+    if (res.status === 200) {
+      const data = await res.json();
+      setUser(data);
+      setIsAuthenticated(true);
+    } else {
+      login();
+    }
+  };
+
+  const login = async () => {
+    if (isRun.current) return;
+    isRun.current = true;
+    keycloak
+      ?.init({
+        onLoad: "check-sso",
+        checkLoginIframe: true,
+        flow: 'hybrid',
+        silentCheckSsoRedirectUri: window.location.origin + '/',
+      })
+      .then((res) => {
+        setIsAuthenticated(res);
+        setCookie("access_token", keycloak?.token);
+        getUserInfo(keycloak?.token as string)
+        
+      });
+    };
+
+  const logout = useCallback(async () => {
+    window.location.href =
+      process.env.NEXT_PUBLIC_KEYCLOAK_URL +
+      `/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/protocol/openid-connect/logout?post_logout_redirect_uri=${window.location.origin}&client_id=${process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID}`;
   }, []);
 
   const handleLogin = () => {
@@ -66,25 +74,30 @@ export function KeycloakProvider({ children }: { children: React.ReactNode }) {
     }
   };
 
-  const logout = async () => {
-    if (keycloak) {
-      try {
-        await keycloak.logout({ redirectUri: window.location.origin });
-        setAuthenticated(false);
-        setUser(null);
-        localStorage.removeItem('user');
-        localStorage.removeItem('authenticated');
-      } catch (err) {
-        console.error('Logout failed:', err);
-      }
+  useEffect(() => {
+    if (!getCookie("access_token")) {
+      login();
+    } else {
+      setToken(getCookie("access_token") as string);
+      getUserInfo(getCookie("access_token") as string);
     }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <KeycloakContext.Provider value={{ keycloak, authenticated, isLoading, logout, user, handleLogin }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        user,
+        handleLogin,
+        token,
+        logout,
+        login,
+      }}
+    >
       {children}
-    </KeycloakContext.Provider>
+    </AuthContext.Provider>
   );
-}
+};
 
-export const useKeycloakContext = () => useContext(KeycloakContext);
+export const useAuth = () => useContext(AuthContext);
