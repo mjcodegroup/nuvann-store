12447c085e3c596b1e417bff37431b219df6a1e1
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2024-08-17 13:56:30 -0300

    feat: prepare base cart and detail page

---
+++ src/pages/details/[id]/view/detail.view.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+import { DetailProps } from '../types';
+
+export default function Details(props: Readonly<DetailProps>) {
+  const { t } = useTranslation();
+  return (
+    <>
+        
+    </>
+  )
+}
---
+++ src/pages/home/components/product/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Styles from './product-card.module.scss';
+import Image from 'next/image';
+
+interface ProductCardProps {
+  product: any;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const router = useRouter();
+
+  const handleCardClick = () => {
+    router.push(`/details/${product.id}`);
+  };
+
+  return (
+    <div className={Styles.see_more_card} key={product.id} onClick={handleCardClick}>
+      <div className={Styles.product_img}>
+        <Image src={product.images[0]} alt="" width={100} height={100} />
+        <Image src={product.images[1]} className={Styles.show_hover} alt="" width={100} height={100} />
+      </div>
+      <div className={Styles.bottom}>
+        <p className={Styles.daily_deal}>Likidasyon</p>
+        <p>
+          <i>de <span className={Styles.lastprice}>{product.prices.before.formatted}</span></i>
+        </p>
+        <p className={Styles.currentPrice}>{product.prices.current.formatted} <span>{product.prices.current.discountPercent} %</span></p>
+        <p className={Styles.description}>
+          {(product.name && product.name.length > 15) ? product.name.substring(0, 15) + '...' : product.name}
+        </p>  
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
