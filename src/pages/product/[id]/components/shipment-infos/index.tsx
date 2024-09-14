6953a39d55470ddea0869abbfbab8ee43c8fa562
marckender <makender103@gmail.com>
2024-09-14 20:35:40 -0300

    refactor: details page ajustes

--- src/pages/product/[id]/components/shipment-infos/index.tsx
+++ src/pages/product/[id]/components/shipment-infos/index.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Styles from './shipment-infos.module.scss';
 import { useTranslation } from 'react-i18next';
+
 interface ShippingInfo {
   id: string;
   delivery_deadline: string;
@@ -17,12 +18,27 @@ interface ShippingProps {
 
 const ShipmentInfos: FC<ShippingProps> = ({ shippingInfos, onInfoSelect }) => {
   const { t } = useTranslation('details');
-  const [selectedId, setSelectedId] = useState<string | null>(null);
+  
+  const [selectedId, setSelectedId] = useState<string | null>(() => {
+    const defaultInfo = shippingInfos?.find((info) => info.default_shipment);
+    return defaultInfo ? defaultInfo.id : null;
+  });
+
+  useEffect(() => {
+    // Define o valor de envio padrão ao carregar as informações
+    if (selectedId) {
+      const selectedInfo = shippingInfos.find((info) => info.id === selectedId);
+      if (selectedInfo) {
+        onInfoSelect(selectedInfo);
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shippingInfos]);
 
   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value)
-    const id = String(event.target.value);
+    const id = event.target.value;
     setSelectedId(id);
+
     const selectedInfo = shippingInfos.find((info) => info.id === id);
     if (selectedInfo) {
       onInfoSelect(selectedInfo);
@@ -34,14 +50,14 @@ const ShipmentInfos: FC<ShippingProps> = ({ shippingInfos, onInfoSelect }) => {
       <h4>{t('details.delivery_information')}</h4>
       <div className={Styles.shipping_list}>
         {shippingInfos?.map((info, index) => (
-          <div key={info.id+index} className={Styles.shipping_info}>
+          <div key={info.id + index} className={Styles.shipping_info}>
             <label htmlFor={`shipping-${info.id}`}>
               <input
                 type="radio"
                 id={`shipping-${info.id}`}
                 name="shipping"
                 value={info.id}
-                checked={info.default_shipment ? true : selectedId === info.id}
+                checked={selectedId === info.id}
                 onChange={handleRadioChange}
               />
               <div className={Styles.info_details}>
--- src/pages/product/[id]/controller/product.controller.tsx
+++ src/pages/product/[id]/controller/product.controller.tsx
@@ -4,20 +4,100 @@ import Product from '../view/product.view'
 import { useParams } from 'next/navigation';
 import { useProducts } from '@/contexts/products';
 import { useProductsInfo } from '@/hooks/use-products-info';
+import { useAuth } from '@/hooks/useKeycloak';
+import { useCartInfo } from '@/hooks/use-cart-info';
+import { useNavigation } from '@/hooks/useNavigation';
+import { RoutesUrls } from '@/utils/enums/routesUrl';
 
 export default function ProductController() {
   const params = useParams<{ id: string; }>()
+  const {redirect} = useNavigation();
   const { state: productDetails, dispatch: productDetailsDispatch } = useProducts();
+  const { isAuthenticated} = useAuth();
+  const {addProductToCart, isLoading: cartLoader} = useCartInfo();
+
+
   const { getProductDetails } = useProductsInfo();
-  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [selectedSize, setSelectedSize] = React.useState({
+    key:"",
+    value : ""
+});
+
+const [selectedShippingInfo, setSelectedShippingInfo] = React.useState({id:0});
+
+const [selectedColor, setSelectedColor] = React.useState({
+key: "",
+value: ""
+});
+const [qty, setQty] = React.useState<number>(1);
+
+  
+const [handleError, sethandleError] = React.useState<boolean>(false)
+
+  const handleSelectColor = (color:any) => {
+    setSelectedColor({
+      key: "color",
+      value:color
+    });
+    sethandleError(false)
+  };
+
+  const handleIncrement = () =>{
+    setQty(qty+1);
+  }
+
+  const handleDecrement = ()=> {
+    if(qty>1) {
+      setQty(qty-1);
+    }
+  }
+
+  const handleChangeQuantity = (qty: number) => {
+    setQty(Number(qty));
+  }
+
+  function handleCartValidation(){
+    const color = !!(productDetails.product?.properties?.color?.length && !selectedColor?.value)
+    const size = !!(productDetails.product?.properties?.size?.length && !selectedSize?.value)
+
+    // if(color || size || !selectedShippingInfo?.id) {
+      if(color || size) {
+        sethandleError(true)
+      return false;
+    } else {
+      sethandleError(false)
+      return true;
+    }
+  }
+
+const handleAddProductToCart = async() => {
+  const ifExist = [
+    selectedSize,
+    selectedColor
+  ]
+  const properties: any = ifExist?.filter(exist=> {
+    return exist.value
+  })
+  if(isAuthenticated) {
+    if(handleCartValidation()) {
+      await addProductToCart({
+       product_id: String(productDetails.product.id),
+       quantity: qty,
+       shipment_id:  selectedShippingInfo.id> 0 ? String(selectedShippingInfo.id) : undefined,
+       properties
+      });
+    }
+  } else {
+      redirect(RoutesUrls.CARTS)
+  }
+}
+
 
   async function getDetailsInformations(id: string) {
     try {
       await getProductDetails(id);
     } catch (error) {
       console.log("algo deu errado")
-    } finally {
-      setIsLoading(false)
     }
   }
 
@@ -27,10 +107,42 @@ export default function ProductController() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params?.id])
 
+  const handleSelectShippingInfo = (selectedShippingInfo: any) => {
+    console.log(selectedShippingInfo)
+    setSelectedShippingInfo(selectedShippingInfo);
+    sethandleError(false)
+  };
+
+  const handleSelectSize = (size: any) => {
+    setSelectedSize({
+      key: "size",
+      value: (size).toString()
+    });
+    sethandleError(false)
+  };
+
 
   return (
     <HomePageDefault>
-      <Product product={productDetails.product} fullLoading={isLoading} />
+      <Product
+        product={productDetails.product}
+        fullLoading={productDetails.isLoading}
+        onSelectedShippingInfo={handleSelectShippingInfo}
+        onSelectedSize={handleSelectSize}
+        sectedSize={selectedSize}
+        onError={handleError}
+        qty={qty}
+        isLoading={cartLoader}
+        selectedShippingInfo={selectedShippingInfo}
+        selectedSize={selectedSize}
+        onSelectedColor={handleSelectColor}
+        selectedColor={selectedColor}
+        onChangeQuantity={handleChangeQuantity}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        onAddToCart={handleAddProductToCart}
+        onPurchase={handleAddProductToCart}
+      />
     </HomePageDefault>
   )
 }
