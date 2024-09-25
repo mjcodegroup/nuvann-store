d7fdcfd8b90e7134a677881633026ed7a0ecc44e
marckender <makender103@gmail.com>
2024-09-24 23:12:47 -0300

    feat: checkout products card styles

--- src/pages/checkout/components/card-products/index.tsx
+++ src/pages/checkout/components/card-products/index.tsx
@@ -5,10 +5,13 @@ import { CardProductsProps } from '../../types';
 import Image from 'next/image';
 import { CheckoutItem } from '@/contexts/checkout/types';
 import { truncateStringWithEllipsis } from '@/utils/truncate-string-with-ellipsis';
+import CustomButton from '@/components/custom-button';
+import { getDefaultShipment } from '@/utils/get-default-shipment';
 
 export default function CardProducts(props: CardProductsProps) {
   const { t } = useTranslation("checkout");
-
+  
+  console.log('props.items', props.items);
   return (
     <div className={Styles.products_wrapper}>
         <h4>{t("checkout.title_products_ordered")}</h4> <br />
@@ -36,13 +39,18 @@ export default function CardProducts(props: CardProductsProps) {
                   <div className={Styles.separator}></div>
                 <div className={Styles._row_footer}>
                   <div>
-                    <h4>{t('checkout.shipping_options')}</h4>
-                    <span>{item.shipping_amount}</span>
+                    <h5>{t('checkout.shipping_options')}:</h5>
                   </div>
-                  {/* <div>
-                    <p>Total Price</p>
-                    <span>{item.total_price}</span>
-                  </div> */}
+                  <div>
+                    <h5>{getDefaultShipment(item?.available_shipments)?.type}</h5>
+                    <span>{getDefaultShipment(item?.available_shipments)?.delivery_deadline}</span>
+                  </div>
+                  <div>
+                    <CustomButton variant='text' title='Change'>Change</CustomButton>
+                  </div>
+                  <div>
+                    <h5>{item.shipping_amount}</h5>
+                  </div>
                 </div>
               </div>
           ))}
