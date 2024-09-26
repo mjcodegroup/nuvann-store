72a6ecb58f8288e78723157d6bfae67aaa424ed3
marckender <makender103@gmail.com>
2024-09-25 23:53:15 -0300

    feat: input validate component , zod, react-hook-form

--- src/pages/checkout/controller/checkout.controller.tsx
+++ src/pages/checkout/controller/checkout.controller.tsx
@@ -1,18 +1,45 @@
-import { HomePageDefault } from '@/components/home-page-default'
 import React from 'react'
+import { HomePageDefault } from '@/components/home-page-default'
+import { SubmitHandler, useForm } from 'react-hook-form';
 import Checkout from '../view/checkout.view'
 import { useCheckoutInfo } from '@/hooks/use-checkout-info';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useUserInfo } from '@/hooks/use-user-info';
+import { z } from 'zod';
+
+type FormValues = {
+  name: string;
+  phone: number;
+}
+
+const schema = z.object({
+  name: z
+  .string()
+  .min(1, "Name is required"),
+  phone: z
+  .string()
+  .min(1, "Phone is required")
+  .regex(/^[0-9]*$/, "Phone must be a number") // Garante que seja um número
+  .transform((val) => parseInt(val)), // Converte para número
+});
 
 export default function CheckoutController() {
   const { checkout } = useCheckoutInfo();
   const {user} = useUserInfo();
 
+  const { register:shipmentAddress, setValue, watch, handleSubmit, formState: { errors,isValid } } = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    mode: "onChange", 
+  });
+
   const [openModalAddress, setOpenModalAddress] = React.useState(false);
 
-  const handleConfirmModalAddress = () => {
-    alert('Address confirmed');
+  const handleConfirmModalAddress:SubmitHandler<FormValues> = data => {
+    console.log('Address confirmed', data);
+    console.log(errors);
   }
+
+  console.log(isValid);
 
   return (
     <HomePageDefault>
@@ -25,10 +52,13 @@ export default function CheckoutController() {
             shipping_cost: checkout.shipping_cost,
             total: checkout.total
           }}
+          shipmentAddress={shipmentAddress}
           openModalAddress={openModalAddress}
           setOpenModalAddress={setOpenModalAddress}
-          disableModalAddressButton={false}
-          onConfirmModalAddress={handleConfirmModalAddress}
+          disableModalAddressButton={!isValid}
+          onConfirmModalAddress={handleSubmit(handleConfirmModalAddress)}
+          shipmentformErrors={errors}
+          setValues={setValue}
           
         />
     </HomePageDefault>
