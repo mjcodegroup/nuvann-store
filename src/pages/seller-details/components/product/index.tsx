43b629aa02fb5cd479d94bae4884671c8ffbebd7
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2024-10-19 21:35:26 -0300

    wip: seller detail  page

--- src/pages/seller-details/components/product/index.tsx
+++ src/pages/seller-details/components/product/index.tsx
@@ -4,27 +4,33 @@ import Image from 'next/image';
 
 interface ProductCardProps {
   product: any;
-  onRedirectToProductDetails:(id: string) => void;
+  onRedirectToProductDetails: (id: string) => void;
 }
 
 const ProductCard: React.FC<ProductCardProps> = (props: ProductCardProps) => {
   const { product } = props;
 
   return (
-    <div className={Styles.see_more_card} key={product.id} onClick={()=>props.onRedirectToProductDetails(product.id)}>
-      <div className={Styles.product_img}>
-        <Image src={product.images[0].url} alt="" width={100} height={100} />
-        <Image src={product.images[1].url} className={Styles.show_hover} alt="" width={100} height={100} />
+    <div className={Styles.see_more_card} key={product.id} onClick={() => props.onRedirectToProductDetails(product.id)}>
+
+      <div className={Styles.top_of_card}>
+        <div className={Styles.product_img}>
+          <Image src={product.images[0].url} alt="" width={100} height={100} />
+          <Image src={product.images[1].url} className={Styles.show_hover} alt="" width={100} height={100} />
+        </div>
+        <div>
+          <p className={Styles.description}>
+            {(product.name && product.name.length > 15) ? product.name.substring(0, 15) + '...' : product.name}
+          </p>
+          <p className={Styles.description}>
+            {(product.description && product.description.length > 15) ? product.description.substring(0, 15) + '...' : product.description}
+          </p>
+        </div>
       </div>
+      {/*  */}
       <div className={Styles.bottom}>
-        <p className={Styles.daily_deal}>Likidasyon</p>
-        <p>
-          <i>de <span className={Styles.lastprice}>{product.prices.original_price.formatted}</span></i>
-        </p>
-        <p className={Styles.currentPrice}>{product.prices.current_price.formatted} <span>{product.prices.current_price.discountPercent} %</span></p>
-        <p className={Styles.description}>
-          {(product.name && product.name.length > 15) ? product.name.substring(0, 15) + '...' : product.name}
-        </p>  
+        <p className={Styles.currentPrice}>{product.prices.current_price.formatted} </p>
+        <p className={Styles.currentPrice}>{product.available_amount} </p>
       </div>
     </div>
   );
