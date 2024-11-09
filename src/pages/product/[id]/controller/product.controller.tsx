809417c3cd32f5d8d48d3bf08e308d4dc38a0d00
marckender <makender103@gmail.com>
2024-11-09 15:38:57 -0300

    fix: bug after login

--- src/pages/product/[id]/controller/product.controller.tsx
+++ src/pages/product/[id]/controller/product.controller.tsx
@@ -22,7 +22,7 @@ export default function ProductController() {
   } = useProductsInfo();
   const { isAuthenticated, loginWithPopup } = useAuth0();
 
-  const { addProductToCart, isRequesting } = useCartInfo({isAuthenticated});
+  const { addProductToCart, isRequesting } = useCartInfo();
   const [ selectedSize, setSelectedSize] = React.useState({} as SizeandProductIE);
   // const [ selectedShippingInfo, setSelectedShippingInfo] = React.useState({id:0});
   const [ selectedColor, setSelectedColor] = React.useState({} as SizeandProductIE);
